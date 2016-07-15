dc849f3dbbc690a56fe9c192836bbae4696981f0
Jose Carlos Valero Sanchez <jsanchez@pivotal.io>
2016-07-15 14:16:40 +0100

    *Delegating into Spring Integration in order to push and retrieve messages to / from JMS. *Proved header propagation using Spring Integration facilities. *Minor code improvement *Missing testing
    Signed-off-by: Joseph Taylor <jtaylor@pivotal.io>

--- src/main/java/JMSChannelBinderConfiguration.java
+++ src/main/java/JMSChannelBinderConfiguration.java
@@ -1,17 +1,15 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 
 public class JMSChannelBinderConfiguration {
 
-    @Autowired
-    ConnectionFactory connectionFactory;
-
     @Bean
-    JMSMessageChannelBinder jmsMessageChannelBinder() throws JMSException {
-        return new JMSMessageChannelBinder(connectionFactory);
+    JMSMessageChannelBinder jmsMessageChannelBinder(JmsTemplate template, ConnectionFactory connectionFactory) throws JMSException {
+        return new JMSMessageChannelBinder(connectionFactory, template);
     }
 
 }
